99d440363d8d87e2379ec07e197cc0f82053facc
Kayla Yu Chen <kayla.chen03@gmail.com>
2025-08-18 14:53:22 -0700

    Create band_name_generator.py

---
+++ Day01/band_name_generator.py
@@ -0,0 +1,11 @@
+# Band Name Generator 🎵
+
+print("🎸 Welcome to the Band Name Generator! 🎤")
+print("I will help you create a fun name for your band.\n")
+
+# Ask the user for inputs
+city = input("🌆 What is the name of your favorite city?\n")
+pet = input("🐶 What is the name of your childhood pet?\n")
+
+# Show the generated band name
+print("\n✨ Your band name could be: " + city + " " + pet + " ✨")
